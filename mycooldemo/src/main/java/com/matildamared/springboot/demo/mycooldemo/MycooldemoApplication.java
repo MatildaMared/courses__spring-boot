c0e47c0d276b7f2de03223b7f2af0b60a03ec5cd
Matilda Mared <matildamared@live.se>
2023-08-23 20:20:53 +0200

    initialize project

---
+++ mycooldemo/src/main/java/com/matildamared/springboot/demo/mycooldemo/MycooldemoApplication.java
@@ -0,0 +1,13 @@
+package com.matildamared.springboot.demo.mycooldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycooldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycooldemoApplication.class, args);
+	}
+
+}
