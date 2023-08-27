cc91b5777badd68415484efe1082e5e339fd84bc
Matilda Mared <matildamared@live.se>
2023-08-27 07:49:51 +0200

    inject custom properties in an endpoint

--- 01-spring-boot-overview/06-properties-demo/src/main/java/com/matildamared/springboot/demo/mycooldemo/rest/FunRestController.java
+++ 01-spring-boot-overview/06-properties-demo/src/main/java/com/matildamared/springboot/demo/mycooldemo/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.matildamared.springboot.demo.mycooldemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject custom properties
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    // Expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + ", Team name: " + teamName;
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "Hello World";
