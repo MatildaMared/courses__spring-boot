86f67b93f44774287334075971dbd5e1d0e49baa
Matilda Mared <matildamared@live.se>
2023-08-28 06:51:37 +0200

    specify what base packages to scan

--- 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/matildamared/springcoredemo/SpringcoredemoApplication.java
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/com/matildamared/springcoredemo/SpringcoredemoApplication.java
@@ -3,7 +3,10 @@ package com.matildamared.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+// with scanBasePackages, we can specify the packages to scan for components, default is the current package
+@SpringBootApplication(scanBasePackages = {
+		"com.matildamared.springcoredemo",
+		"com.matildamared.util"})
 public class SpringcoredemoApplication {
 
 	public static void main(String[] args) {
