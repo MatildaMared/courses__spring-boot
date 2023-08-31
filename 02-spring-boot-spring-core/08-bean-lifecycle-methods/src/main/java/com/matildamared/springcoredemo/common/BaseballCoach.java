c99d7482b4a0c9348307466f2cad6a331f2a748c
Matilda Mared <matildamared@live.se>
2023-08-31 06:50:30 +0200

    add post construct and pre destroy methods

--- 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/matildamared/springcoredemo/common/BaseballCoach.java
+++ 02-spring-boot-spring-core/08-bean-lifecycle-methods/src/main/java/com/matildamared/springcoredemo/common/BaseballCoach.java
@@ -1,5 +1,7 @@
 package com.matildamared.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -7,6 +9,16 @@ public class BaseballCoach implements Coach {
 
     public BaseballCoach() {
         System.out.println("In constructor " + getClass().getSimpleName());
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff() " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff() " + getClass().getSimpleName());
     }
 
     @Override
