2b2380cf8b239fadf25002d160e199e314f0b2f9
Matilda Mared <matildamared@live.se>
2023-08-31 07:07:31 +0200

    add configuratin for swim coach

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/matildamared/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package com.matildamared.springcoredemo.config;
+
+import com.matildamared.springcoredemo.common.Coach;
+import com.matildamared.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach();
+    }
+}
