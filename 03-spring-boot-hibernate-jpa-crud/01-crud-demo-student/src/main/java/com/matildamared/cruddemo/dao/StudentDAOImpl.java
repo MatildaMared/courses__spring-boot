f3edd7bc83108c35b6adf586f20c6824adf46f09
Matilda Mared <matildamared@live.se>
2023-09-11 07:02:50 +0200

    add method to delete all students

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/matildamared/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/com/matildamared/cruddemo/dao/StudentDAOImpl.java
@@ -57,4 +57,11 @@ public class StudentDAOImpl implements StudentDAO {
         Student student = entityManager.find(Student.class, id);
         entityManager.remove(student);
     }
+
+    @Override
+    @Transactional
+    public Integer deleteAll() {
+        Integer numRowsDeleted = entityManager.createQuery("DELETE FROM Student").executeUpdate();
+        return numRowsDeleted;
+    }
 }
